ed78964a93a0b24dc58335893fa3c718649b859f
Moroj-Alharbi <moroj.alh@gmail.com>
2019-02-02 15:44:42 +0300

    add logs

--- dafoor/components/Logs.js
+++ dafoor/components/Logs.js
@@ -1,22 +1,114 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import firebase from 'firebase';
 // import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
-export default class logs extends React.Component {
+const config = require('./firebase/config');
+
+
+export default class Logs extends React.Component {
     constructor(){
       super();
       this.state = {
         typeOfLog: '',
         userType: '',
-        activePage: ''
+        activePage: '',
+        email: '',
+        password: '',
+        errorMessage: null
       }
     }
 
+    componentWillMount(){
+      firebase.initializeApp(config);
+    }
 
+    handleSignUp = () => {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(() => {
+          console.log('User sign up!!!!');
+        })
+        .catch(error => this.setState({ errorMessage: error.message }))
+    }
+
+    handleSignIn = () => {
+      firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then(() => {
+        console.log("User sign in!!!!");
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage)
+        // ...
+      });
+  
+    }
+
+    renderLogs(){
+      if (this.state.typeOfLog === 'signup') {
+        if (this.state.activePage === 'profile') {
+          return (
+            <div>
+
+            </div>
+          )
+        }
+        return (
+          <View>
+            <Text>
+            SIGN UP
+            </Text>
+            <TextInput
+              placeholder="Email"
+              autoCapitalize="none"
+              // style={styles.textInput}
+              onChangeText={email => this.setState({ email })}
+              value={this.state.email}
+            />
+            <TextInput
+              secureTextEntry
+              placeholder="Password"
+              autoCapitalize="none"
+              // style={styles.textInput}
+              onChangeText={password => this.setState({ password })}
+              value={this.state.password}
+            />
+            <Button title="Sign Up" onPress={this.handleSignUp} />
+          </View>
+        )
+      } else {
+        return (
+          <View>
+            <Text>Sign in!</Text>
+            <TextInput
+              placeholder="Email"
+              autoCapitalize="none"
+              // style={styles.textInput}
+              onChangeText={email => this.setState({ email })}
+              value={this.state.email}
+            />
+            <TextInput
+              secureTextEntry
+              placeholder="Password"
+              autoCapitalize="none"
+              // style={styles.textInput}
+              onChangeText={password => this.setState({ password })}
+              value={this.state.password}
+            />
+            <Button title="Sign in" onPress={this.handleSignIn} />
+          </View>
+        )
+      }
+    }
     render() {
       return (
         <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
+          <Text>LOGS</Text>
+            {this.renderLogs()}
         </View>
       );
     }
