ce107b6a768abbbb8854ee9be1f1a344454da644
Moroj-Alharbi <moroj.alh@gmail.com>
2019-02-03 14:15:09 +0300

    add map

--- dafoor/components/student/Map.js
+++ dafoor/components/student/Map.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class Map extends React.Component {
     constructor(){
@@ -8,11 +9,27 @@ export default class Map extends React.Component {
       }
     }
 
+    renderMap(){
+      return (
+        <View>
+          <Text>ksnda</Text>
+          <MapView
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+        </View>
+      )
+    }
 
     render() {
       return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>MAP!!!!!</Text>
+          {/* <Text>MAP!!!!!</Text> */}
+          {this.renderMap()}
         </View>
       );
     }
